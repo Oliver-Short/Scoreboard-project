9adfa0568accb9c367139aee7e9841974ae1f11e
Oliver-Short <oliverjamesshort@gmail.com>
2025-08-05 18:57:24 +0100

    Commit to main
    first time using github

---
+++ Scrimba Scoreboard/index.js
@@ -0,0 +1,28 @@
+let homeScoreEl = document.getElementById("home-score")
+let guestScoreEl = document.getElementById("guest-score")
+
+let homeScore = 0
+let guestScore = 0
+
+updateScore()
+
+function updateScore() {
+    homeScoreEl.textContent = homeScore
+    guestScoreEl.textContent = guestScore
+}
+
+function homeIncrease(value) {
+    homeScore += value
+    updateScore()
+}
+
+function guestIncrease(value) {
+    guestScore += value
+    updateScore()
+}
+
+function newGame() {
+    homeScore = 0
+    guestScore = 0
+    updateScore()
+}
